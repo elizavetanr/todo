d7b25623f854548719dd830116ed580e0f5f6a06
elizaveta <elizaveta.nurzhanova@gmail.com>
2022-02-26 22:52:30 +0300

    todo with interface

--- script.js
+++ script.js
@@ -11,29 +11,18 @@ const PRIORITY = {
     HIGH: 'high',
 };
 
-const DEFAULT_PRIORITY = PRIORITY.LOW
-
-const list = [
-    { name: 'create a post', status: STATUS.IN_PROGRESS, priority: PRIORITY.LOW },
-    { name: 'test', status: STATUS.DONE, priority: PRIORITY.HIGH }
-]
+const list = [];
 
 let newList;
+let index;
 
-function changeStatus(name, status) {
+function changeStatus(name, status, priority) {
     findIndex(name);
-    let priority = list[index].priority;
-    list.splice(index, 1, { name, status, priority });
+    list.splice(index, 1, { name, status, priority});
 }
 
-function changePriority(name, priority) {
-    findIndex(name);
-    let status = list[index].status;
-    list.splice(index, 1, { name, status, priority });
-}
-
-function addTask(name) {
-    list.push({ name, status: DEFAULT_STATUS, priority: DEFAULT_PRIORITY });
+function addTask(name, priority) {
+    list.push({ name, status: DEFAULT_STATUS, priority });
 }
 
 function deleteTask(name) {
@@ -41,32 +30,6 @@ function deleteTask(name) {
     list.splice(index, 1);
 }
 
-function showBy(style) {
-    if (style === 'status') {
-        console.log(STATUS.TO_DO + ':');
-        createNewListByStatus(STATUS.TO_DO);
-        showName(newList);
-
-        console.log(STATUS.IN_PROGRESS + ':');
-        createNewListByStatus(STATUS.IN_PROGRESS);
-        showName(newList);
-
-        console.log(STATUS.DONE + ':');
-        createNewListByStatus(STATUS.DONE);
-        showName(newList);
-    }
-
-    if (style === 'priority') {
-        console.log(PRIORITY.HIGH + ':');
-        createNewListByPriority(PRIORITY.HIGH);
-        showName(newList);
-
-        console.log(PRIORITY.LOW + ':');
-        createNewListByPriority(PRIORITY.LOW);
-        showName(newList);
-
-    }
-}
 
 function findIndex(name) {
     index = (list.findIndex(function (task) {
@@ -74,31 +37,160 @@ function findIndex(name) {
     }));
 };
 
-function createNewListByStatus(status) {
-    newList = list.filter(function (task) {
-        return task.status === status
-    });
-}
-
 function createNewListByPriority(priority) {
     newList = list.filter(function (task) {
         return task.priority === priority
     });
 }
 
-function showName(newList) {
-    if (newList.length === 0) {
-      console.log('-');
-    } else {
-      newList.forEach(function func(item) {
-        console.log(item.name)
-      });
-    }
-  }
+const DEFAULT_VALUE_HIGH = 'Добавить важных дел';
+const DEFAULT_VALUE_LOW = 'Добавить';
 
-changeStatus('create a post', 'Done');
-addTask('cleaning')
-deleteTask('test')
-showBy('status')
-//changePriority('cleaning', 'high')
-//showBy('priority')
+formHigh.addEventListener("click", cleanDefaultValueHigh);
+formHigh.addEventListener("submit", addHighTask);
+formHigh.addEventListener("submit", defaultValueHigh);
+formHigh.addEventListener("submit", cleanHighTasks);
+formHigh.addEventListener("submit", outputHighTasks);
+
+formLow.addEventListener("click", cleanDefaultValueLow);
+formLow.addEventListener("submit", addLowTask);
+formLow.addEventListener("submit", defaultValueLow);
+formLow.addEventListener("submit", cleanLowTasks);
+formLow.addEventListener("submit", outputLowTasks);
+
+function cleanDefaultValueHigh() {
+    if (document.body.children[0].children[0].children[1].children[0].value === DEFAULT_VALUE_HIGH) {
+        document.body.children[0].children[0].children[1].children[0].value = ''; };
+};
+
+function addHighTask() {
+    let name = document.body.children[0].children[0].children[1].children[0].value;
+    addTask(name, PRIORITY.HIGH);
+}
+
+function defaultValueHigh() {
+    document.body.children[0].children[0].children[1].children[0].value = DEFAULT_VALUE_HIGH;
+}
+
+function cleanHighTasks() {
+    highTaskList.innerHTML = '';
+}
+
+function outputHighTasks() {
+    createNewListByPriority(PRIORITY.HIGH);
+    const HTMLpart1 = '<div class="task-high-list"><div className="checkbox"><label><input type="checkbox" onclick="changeStatusHighPriority(), colorHighTaskDone()" class="checkboxHigh">';
+    const HTMLpart2 = '</label></div><button class="del-high-task"><img src="del-icon.png" class="del-icon"></button></div>';
+    for (let i = 0; i < newList.length; i++) {
+        highTaskList.innerHTML += HTMLpart1 + newList[i].name + HTMLpart2;
+    }
+    outputHighCheck();
+    setButtonsDel();
+
+}
+
+function cleanDefaultValueLow() {
+    if (document.body.children[0].children[1].children[1].children[0].value === DEFAULT_VALUE_LOW) {
+        document.body.children[0].children[1].children[1].children[0].value = ''; };
+};
+
+function addLowTask() {
+    let name = document.body.children[0].children[1].children[1].children[0].value;
+    addTask(name, PRIORITY.LOW);
+}
+
+function defaultValueLow() {
+    document.body.children[0].children[1].children[1].children[0].value = DEFAULT_VALUE_LOW;
+}
+
+function cleanLowTasks() {
+    lowTaskList.innerHTML = '';
+}
+
+function outputLowTasks() {
+    createNewListByPriority(PRIORITY.LOW);
+    const HTMLpart1 = '<div class="task-low-list"><div className="checkbox"><input type="checkbox" onclick="changeStatusLowPriority(), colorLowTaskDone()" class="checkboxLow">';
+    const HTMLpart2 = '</div><button class="del-low-task"><img src="del-icon.png" class="del-icon"></button></div>';
+    for (let i = 0; i < newList.length; i++) {
+        lowTaskList.innerHTML += HTMLpart1 + newList[i].name + HTMLpart2;
+    }
+    outputLowCheck();
+    setButtonsDel();
+}
+
+
+function setButtonsDel() {
+    let tasks = document.querySelectorAll("button.del-high-task, button.del-low-task");
+    for (let elem of tasks) {
+        elem.addEventListener("click", function () {
+            elem.parentNode.remove();
+            deleteTask(elem.textContent);
+        });
+    }
+}
+
+function changeStatusHighPriority() {
+    let checkboxes = document.querySelectorAll("input.checkboxHigh");
+    for (let elem of checkboxes) {
+        if (elem.checked) {
+            changeStatus((elem.parentNode.textContent), STATUS.DONE, PRIORITY.HIGH);
+        } else {
+            changeStatus((elem.parentNode.textContent), STATUS.TO_DO, PRIORITY.HIGH);
+        }
+    };
+};
+
+function changeStatusLowPriority() {
+    let checkboxes = document.querySelectorAll("input.checkboxLow");
+    for (let elem of checkboxes) {
+        if (elem.checked) {
+            changeStatus((elem.parentNode.textContent), STATUS.DONE, PRIORITY.LOW);
+        } else {
+            changeStatus((elem.parentNode.textContent), STATUS.TO_DO, PRIORITY.LOW);
+        }
+    };
+};
+
+function outputHighCheck() {
+    createNewListByPriority(PRIORITY.HIGH);
+    for (let i = 0; i < newList.length; i++) {
+            if (newList[i].status === STATUS.DONE) {
+                document.querySelectorAll("input.checkboxHigh")[i].checked = true;
+            }
+    }
+
+}
+function outputLowCheck() {
+    createNewListByPriority(PRIORITY.LOW);
+    for (let i = 0; i < newList.length; i++) {
+        if (newList[i].status === STATUS.DONE) {
+            document.querySelectorAll("input.checkboxLow")[i].checked = true;
+        }
+    }
+}
+function colorHighTaskDone() {
+    createNewListByPriority(PRIORITY.HIGH);
+    for (let i = 0; i < newList.length; i++) {
+        if (newList[i].status === STATUS.DONE) {
+            document.querySelectorAll("div.task-high-list")[i].className = 'task-high-list done-high-task';
+            document.querySelectorAll("button.del-high-task")[i].className = 'del-high-task del-done-high-task';
+        } else {
+            document.querySelectorAll("div.task-high-list")[i].className = 'task-high-list';
+            document.querySelectorAll("button.del-high-task")[i].className = 'del-high-task';
+        }
+    }
+
+}
+
+function colorLowTaskDone() {
+    createNewListByPriority(PRIORITY.LOW);
+    for (let i = 0; i < newList.length; i++) {
+        if (newList[i].status === STATUS.DONE) {
+            document.querySelectorAll("div.task-low-list")[i].className = 'task-low-list done-low-task';
+            document.querySelectorAll("button.del-low-task")[i].className = 'del-low-task del-done-low-task';
+        } else {
+            document.querySelectorAll("div.task-low-list")[i].className = 'task-low-list';
+            document.querySelectorAll("button.del-low-task")[i].className = 'del-low-task';
+        }
+    }
+
+}
